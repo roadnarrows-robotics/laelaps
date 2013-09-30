136015388c6f8a6c13406c1dd95c955515957a71
root <root@laelaps.(none)>
2013-09-30 16:56:44 +0000

    adding changed msg files and launch files

--- laelaps_control/src/laelaps_control.cpp
+++ laelaps_control/src/laelaps_control.cpp
@@ -1,4 +1,49 @@
-int main(int argc, char* argv[])
+#include <string>
+#include "ros/ros.h"
+
+using namespace std;
+
+bool SetSpeed(mot::SetSpeed::Request &req,
+              mot::SetSpeed::Response &rsp)
 {
+  ROS_INFO("Setting speed"):
+    pMot->setSpeed(req.devName, req.mot_id, req.speed);
+    fprintf(stderr, "Speed = %f \n", req,speed);
+  retrun true;
+}
+
+int main(int argc, char* argv[])
+{ 
+  LOG_SET_THRESHOLD(LOG_LEVEL_DIAG3);
+  ros::init(argc, argv, "laelaps_control");
+  ros::NodeHandle n("laelaps_control");
+
+  pMot=new MotRoboteqSmall;
+
+  string devName1="/dev/ttyACM0";
+  string devName2="/dev/ttyACM1";
+  int baudRate=115200;
+  if(pMot->open(devName, baudRate)!=0)
+  {
+    fprintf(stderr, "jnt failed to open\n");
+    return -1;
+  }
+  
+  //Services
+  ros::ServiceServer set_speed_ser = n.advertiseService("set_speed", 
+                                                       SetSpeed);
+
+  //Subscriptions
+  ros::Subscriber speed_command_sub = n.subscribe("speed_command", 1,
+                                                 speed_commandCB);
+
+  ros::spin();
+
+  if(pMot->close()!=0)
+  {
+    fprintf(stderr, "jnt failed to close\n");
+    return -1;
+  }
+
   return 0;
 }
